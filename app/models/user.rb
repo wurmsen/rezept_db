9569ee46218e45a3859440bdadedf5b3777298ab
Thomas Wurm <webmaster@wurmsen.net>
2010-08-12 22:35:21 +0200

    first steps of recipe styles

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   acts_as_authentic
   validates_presence_of :first_name, :last_name
   has_many :recipes
+
+  def display_name
+    [first_name, last_name].compact.join(" ")
+  end
 end
